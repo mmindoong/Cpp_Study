d2d02427c79589fb49bd60ad8127c44bcebbf873
mmindoong <70145314+mmindoong@users.noreply.github.com>
2021-02-24 11:45:53 +0900

    Update 2739.cpp

--- 2739.cpp
+++ 2739.cpp
@@ -4,16 +4,11 @@ using namespace std;
 int main()
 {
 	int n;
-
 	cin >> n;
 
-	if (n < 1 || n > 9)
-		return 0;
-
-	for (int i = 1; i <= 9; i++)
+	for (int i = 1; i < 10; i++)
 	{
-		cout << n << " * " << i << " = " << n * i << endl;
+		cout << n << " * " << i << " = " << n * i << "\n" ;
 	}
-
 	return 0;
 }
