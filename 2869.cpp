5dc8a57af6acdd4439939ef0c5094de46b4ec832
mmindoong <70145314+mmindoong@users.noreply.github.com>
2021-02-25 16:15:23 +0900

    [Baekjoon]Baekjoon0225.cpp

---
+++ 2869.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A, B, V, n;
+    cin >> A >> B >> V;
+    if ((V - A) % (A - B) == 0)
+        n = (V - A) / (A - B) + 1;
+    else
+        n = (V - A) / (A - B) + 2;
+    cout << n << endl;
+    return 0;
+}
